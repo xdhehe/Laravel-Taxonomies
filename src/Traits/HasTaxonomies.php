14252170e339c7ec7be351c79492e40cd4b4d901
Learner <26359059+xdhehe@users.noreply.github.com>
2019-06-17 18:00:30 +0800

    Update HasTaxonomies.php

--- src/Traits/HasTaxonomies.php
+++ src/Traits/HasTaxonomies.php
@@ -5,42 +5,64 @@ use Lecturize\Taxonomies\Models\Taxonomy;
 use Lecturize\Taxonomies\Models\Term;
 use Lecturize\Taxonomies\TaxableUtils;
 
+use DB;
 /**
  * Class HasTaxonomies
  * @package Lecturize\Taxonomies\Traits
  */
 trait HasTaxonomies
 {
-    /**
-     * Return a collection of taxonomies related to the taxed model.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
-     */
-    public function taxed()
-    {
-        return $this->morphMany(Taxable::class, 'taxable');
-    }
+	/**
+	 * Return a collection of taxonomies related to the taxed model.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+	 */
+	public function taxed()
+	{
+		return $this->morphMany(Taxable::class, 'taxable');
+	}
 
-    /**
-     * Return a collection of taxonomies related to the taxed model.
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
-     */
-    public function taxonomies()
-    {
-        return $this->morphToMany(Taxonomy::class, 'taxable');
-    }
+	/**
+	 * Return a collection of taxonomies related to the taxed model.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\MorphToMany
+	 */
+	public function taxonomies()
+	{
+		return $this->morphToMany(Taxonomy::class, 'taxable');
+	}
 
-    /**
-     * Add one or multiple terms in a given taxonomy.
-     *
-     * @param mixed    $terms
-     * @param string   $taxonomy
-     * @param integer  $parent
-     * @param integer  $order
-     */
-    public function addTerm($terms, $taxonomy, $parent = 0, $order = 0)
-    {
+	/**
+	 * Add one or multiple terms in a given taxonomy.
+	 *
+	 * @param mixed    $terms
+	 * @param string   $taxonomy
+	 * @param integer  $parent
+	 * @param integer  $order
+	 */
+	// public function addTerm($terms, $taxonomy, $parent = 0, $order = 0) {
+	// 	$terms = TaxableUtils::makeTermsArray($terms);
+
+	// 	$this->createTaxables($terms, $taxonomy, $parent, $order);
+
+	// 	$terms = Term::whereIn('name', $terms)->pluck('id')->all();
+
+	// 	if (count($terms) > 0) {
+	// 		foreach ($terms as $term) {
+	// 			if ($this->taxonomies()->where('taxonomy', $taxonomy)->where('term_id', $term)->first())
+	// 				continue;
+
+	// 			$tax = Taxonomy::where('term_id', $term)->first();
+	// 			$this->taxonomies()->attach($tax->id);
+	// 		}
+
+	// 		return;
+	// 	}
+
+	// 	$this->taxonomies()->detach();
+	// }
+
+	public function addTerm($terms, $taxonomy, $parent = 0, $order = 0) {
         $terms = TaxableUtils::makeTermsArray($terms);
 
         $this->createTaxables($terms, $taxonomy, $parent, $order);
@@ -51,229 +73,245 @@ trait HasTaxonomies
             foreach ($terms as $term) {
                 if ($this->taxonomies()->where('taxonomy', $taxonomy)->where('term_id', $term)->first())
                     continue;
-
-                $tax = Taxonomy::where('term_id', $term)->first();
+                $tax = Taxonomy::where('term_id', $term)->where('taxonomy', $taxonomy)->first();
                 $this->taxonomies()->attach($tax->id);
             }
-
             return;
         }
 
         $this->taxonomies()->detach();
     }
 
-    /**
-     * Convenience method for attaching this models taxonomies to the given parent taxonomy.
-     *
-     * @param  integer  $taxonomy_id
-     */
-    public function setCategory($taxonomy_id)
-    {
-        $this->taxonomies()->attach($taxonomy_id);
-    }
+	/**
+	 * Convenience method for attaching this models taxonomies to the given parent taxonomy.
+	 *
+	 * @param  integer  $taxonomy_id
+	 */
+	public function setCategory($taxonomy_id)
+	{
+		$this->taxonomies()->attach($taxonomy_id);
+	}
 
-    /**
-     * Create terms and taxonomies (taxables).
-     *
-     * @param mixed    $terms
-     * @param string   $taxonomy
-     * @param integer  $parent
-     * @param integer  $order
-     */
-    public function createTaxables($terms, $taxonomy, $parent = 0, $order = 0)
-    {
-        $terms = TaxableUtils::makeTermsArray($terms);
+	/**
+	 * Create terms and taxonomies (taxables).
+	 *
+	 * @param mixed    $terms
+	 * @param string   $taxonomy
+	 * @param integer  $parent
+	 * @param integer  $order
+	 */
+	public function createTaxables($terms, $taxonomy, $parent = 0, $order = 0)
+	{
+		$terms = TaxableUtils::makeTermsArray($terms);
 
-        TaxableUtils::createTerms($terms);
-        TaxableUtils::createTaxonomies($terms, $taxonomy, $parent, $order);
-    }
+		TaxableUtils::createTerms($terms );
+		TaxableUtils::createTaxonomies($terms, $taxonomy, $parent, $order );
+	}
 
-    /**
-     * Pluck taxonomies by given field.
-     *
-     * @param  string  $by
-     * @return mixed
-     */
-    public function getTaxonomies($by = 'id')
-    {
-        return $this->taxonomies->pluck($by);
-    }
+	/**
+	 * Pluck taxonomies by given field.
+	 *
+	 * @param  string  $by
+	 * @return mixed
+	 */
+	public function getTaxonomies($by = 'id')
+	{
+		return $this->taxonomies->pluck($by);
+	}
 
-    /**
-     * Pluck terms for a given taxonomy by name.
-     *
-     * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function getTermNames($taxonomy = '')
-    {
-        if ($terms = $this->getTerms($taxonomy))
+	/**
+	 * Pluck terms for a given taxonomy by name.
+	 *
+	 * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function getTermNames($taxonomy = '')
+	{
+		if ($terms = $this->getTerms($taxonomy))
             $terms->pluck('name');
 
-        return null;
-    }
+		return null;
+	}
 
-    /**
-     * Get the terms related to a given taxonomy.
-     *
-    * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function getTerms($taxonomy = '')
-    {
+	/**
+	 * Get the terms related to a given taxonomy.
+	 *
+     * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function getTerms($taxonomy = '',$parent = -1)
+	{
+		if (($taxonomy) && ($parent!=-1)) {
+			$term_ids = $this->taxonomies->where('taxonomy', $taxonomy)->where('parent', $parent)->pluck('term_id');
+		} 
+		else if ($taxonomy){
+			$term_ids = $this->taxonomies->where('taxonomy', $taxonomy)->pluck('term_id');
+		}
+		else {
+			$term_ids = $this->getTaxonomies('term_id');
+		}
+
+		return Term::whereIn('id', $term_ids)->get();
+	}
+
+	/**
+	 * Get a term model by the given name and optionally a taxonomy.
+	 *
+	 * @param  string  $term_name
+     * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function getTerm($term_name, $taxonomy = '')
+	{
         if ($taxonomy) {
-            $term_ids = $this->taxonomies->where('taxonomy', $taxonomy)->pluck('term_id');
-        } else {
-            $term_ids = $this->getTaxonomies('term_id');
-        }
+			$term_ids = $this->taxonomies->where('taxonomy', $taxonomy)->pluck('term_id');
+		} else {
+			$term_ids = $this->getTaxonomies('term_id');
+		}
 
-        return Term::whereIn('id', $term_ids)->get();
-    }
+		return Term::whereIn('id', $term_ids)->where('name', $term_name)->first();
+	}
 
-    /**
-     * Get a term model by the given name and optionally a taxonomy.
-     *
-     * @param  string  $term_name
-    * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function getTerm($term_name, $taxonomy = '')
-    {
-        if ($taxonomy) {
-            $term_ids = $this->taxonomies->where('taxonomy', $taxonomy)->pluck('term_id');
-        } else {
-            $term_ids = $this->getTaxonomies('term_id');
-        }
+	/**
+	 * Check if this model has a given term.
+	 *
+	 * @param  string  $term_name
+	 * @param  string  $taxonomy
+	 * @return boolean
+	 */
+	public function hasTerm($term_name, $taxonomy = '')
+	{
+		return (bool) $this->getTerm($term_name, $taxonomy);
+	}
 
-        return Term::whereIn('id', $term_ids)->where('name', $term_name)->first();
-    }
+	/**
+	 * Disassociate the given term from this model.
+	 *
+	 * @param  string  $term_name
+	 * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function removeTerm($term_name, $taxonomy = '')
+	{
+		if (! $term = $this->getTerm($term_name, $taxonomy))
+			return null;
 
-    /**
-     * Check if this model has a given term.
-     *
-     * @param  string  $term_name
-     * @param  string  $taxonomy
-     * @return boolean
-     */
-    public function hasTerm($term_name, $taxonomy = '')
-    {
-        return (bool) $this->getTerm($term_name, $taxonomy);
-    }
+		if ($taxonomy) {
+			$taxonomy = $this->taxonomies->where('taxonomy', $taxonomy)->where('term_id', $term->id)->first();
+		} else {
+			$taxonomy = $this->taxonomies->where('term_id', $term->id)->first();
+		}
 
-    /**
-     * Disassociate the given term from this model.
-     *
-     * @param  string  $term_name
-     * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function removeTerm($term_name, $taxonomy = '')
-    {
-        if (! $term = $this->getTerm($term_name, $taxonomy))
-            return null;
+		return $this->taxed()->where('taxonomy_id', $taxonomy->id)->delete();
+	}
 
-        if ($taxonomy) {
-            $taxonomy = $this->taxonomies->where('taxonomy', $taxonomy)->where('term_id', $term->id)->first();
-        } else {
-            $taxonomy = $this->taxonomies->where('term_id', $term->id)->first();
-        }
+	/**
+	 * Disassociate all terms from this model.
+	 *
+	 * @return mixed
+	 */
+	public function removeAllTerms()
+	{
+		return $this->taxed()->delete();
+	}
 
-        return $this->taxed()->where('taxonomy_id', $taxonomy->id)->delete();
-    }
+	/**
+	 * Scope by given terms.
+	 *
+	 * @param  object  $query
+	 * @param  array   $terms
+	 * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function scopeWithTerms($query, $terms, $taxonomy)
+	{
+		$terms = TaxableUtils::makeTermsArray($terms);
 
-    /**
-     * Disassociate all terms from this model.
-     *
-     * @return mixed
-     */
-    public function removeAllTerms()
-    {
-        return $this->taxed()->delete();
-    }
+		foreach ($terms as $term)
+			$this->scopeWithTerm($query, $term, $taxonomy);
 
-    /**
-     * Scope by given terms.
-     *
-     * @param  object  $query
-     * @param  array   $terms
-     * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function scopeWithTerms($query, $terms, $taxonomy)
-    {
-        $terms = TaxableUtils::makeTermsArray($terms);
+		return $query;
+	}
 
-        foreach ($terms as $term)
-            $this->scopeWithTerm($query, $term, $taxonomy);
+	/**
+	 * Scope by the given term.
+	 *
+	 * @param  object  $query
+	 * @param  string  $term_name
+	 * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function scopeWithTerm($query, $term_name, $taxonomy)
+	{
+		$term_ids = Taxonomy::where('taxonomy', $taxonomy)->pluck('term_id');
 
-        return $query;
-    }
+		$term     = Term::whereIn('id', $term_ids)->where('name', $term_name)->first();
+		$taxonomy = Taxonomy::where('term_id', $term->id)->first();
 
-    /**
-     * Scope by the given term.
-     *
-     * @param  object  $query
-     * @param  string  $term_name
-     * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function scopeWithTerm($query, $term_name, $taxonomy)
-    {
-        $term_ids = Taxonomy::where('taxonomy', $taxonomy)->pluck('term_id');
+		return $query->whereHas('taxonomies', function($q) use($term, $taxonomy) {
+			$q->where('term_id', $term->id);
+		});
+	}
 
-        $term     = Term::whereIn('id', $term_ids)->where('name', $term_name)->first();
-        $taxonomy = Taxonomy::where('term_id', $term->id)->first();
+	/**
+	 * Scope by given taxonomy.
+	 *
+	 * @param  object  $query
+	 * @param  string  $term_name
+	 * @param  string  $taxonomy
+	 * @return mixed
+	 */
+	public function scopeWithTax($query, $term_name, $taxonomy)
+	{
+		$term_ids = Taxonomy::where('taxonomy', $taxonomy)->pluck('term_id');
 
-        return $query->whereHas('taxonomies', function($q) use($term, $taxonomy) {
-            $q->where('term_id', $term->id);
-        });
-    }
+		$term     = Term::whereIn('id', $term_ids)->where('name', $term_name)->first();
+		$taxonomy = Taxonomy::where('term_id', $term->id)->first();
 
-    /**
-     * Scope by given taxonomy.
-     *
-     * @param  object  $query
-     * @param  string  $term_name
-     * @param  string  $taxonomy
-     * @return mixed
-     */
-    public function scopeWithTax($query, $term_name, $taxonomy)
-    {
-        $term_ids = Taxonomy::where('taxonomy', $taxonomy)->pluck('term_id');
+		return $query->whereHas('taxed', function($q) use($term, $taxonomy) {
+			$q->where('taxonomy_id', $taxonomy->id);
+		});
+	}
 
-        $term     = Term::whereIn('id', $term_ids)->where('name', $term_name)->first();
-        $taxonomy = Taxonomy::where('term_id', $term->id)->first();
+	/**
+	 * Scope by category id.
+	 *
+	 * @param  object   $query
+	 * @param  integer  $taxonomy_id
+	 * @return mixed
+	 */
+	public function scopeHasCategory($query, $taxonomy_id)
+	{
+		return $query->whereHas('taxed', function($q) use($taxonomy_id) {
+			$q->where('taxonomy_id', $taxonomy_id);
+		});
+	}
 
-        return $query->whereHas('taxed', function($q) use($term, $taxonomy) {
-            $q->where('taxonomy_id', $taxonomy->id);
-        });
-    }
+	public function getParentName($parentId){
+		$name = DB::table('taxonomies')
+		    ->rightJoin('terms', 'taxonomies.parent', '=', 'terms.id')
+		    ->where('parent', $parentId)
+		    ->first();		                 
+		    
+	    return $name;
+	}
+	
+	public function getChildren($taxonomy='',$parent=-1){
+		if ($parent!= -1){
+        	$child = DB::table('taxonomies')
+                 ->rightJoin('terms', 'taxonomies.term_id', '=', 'terms.id')
+                 ->where('taxonomy', $taxonomy)
+                 ->where('parent', $parent)
+                 ->get();					
+		}
+// getting the terms
+		else{
+        	$child = DB::table('taxonomies')
+                 ->rightJoin('terms', 'taxonomies.term_id', '=', 'terms.id')
+                 ->where('taxonomy', $taxonomy)
+                 ->get();		
+     	}
+         return $child;
+	}	
 
-    /**
-     * Scope by category id.
-     *
-     * @param  object   $query
-     * @param  integer  $taxonomy_id
-     * @return mixed
-     */
-    public function scopeHasCategory($query, $taxonomy_id)
-    {
-        return $query->whereHas('taxed', function($q) use($taxonomy_id) {
-            $q->where('taxonomy_id', $taxonomy_id);
-        });
-    }
-
-    /**
-     * Scope by category ids.
-     *
-     * @param  object  $query
-     * @param  array   $taxonomy_ids
-     * @return mixed
-     */
-    public function scopeHasCategories($query, $taxonomy_ids)
-    {
-        return $query->whereHas('taxed', function($q) use($taxonomy_ids) {
-            $q->whereIn('taxonomy_id', $taxonomy_ids);
-        });
-    }
 }
